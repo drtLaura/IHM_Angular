403b71110e4376b20d0d3259ab2484bdf668ace9
AurorePhl <asiphilippe@hotmail.com>
2024-12-12 09:08:50 +0100

    update

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,22 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FriendsComponent } from "../friends/friends.component";
 import { MessagesComponent } from "../messages/messages.component";
+import { ChatService } from '../services/chat.service';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [FriendsComponent, MessagesComponent],
+  imports: [FriendsComponent, MessagesComponent,CommonModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css'] // Correction : styleUrls au lieu de styleUrl
 })
-export class ChatComponent {
-  showFriends: boolean;  // Contrôle si app-friends est visible ou non
+export class ChatComponent implements OnInit {
+  // Contrôle si app-friends est visible ou non
+  showFriends: boolean = false;
 
-  constructor() {
-    this.showFriends = false;
+  // Contrôle si app-messages est visible ou non
+  showMessages: boolean = false;
+
+  // Liste des chats avec la structure définie
+  chats: { 
+    id: number; 
+    users: { id: number; username: string; }[]; 
+    name: string; 
+    messages: { 
+      id: number; 
+      user: { id: number; username: string; }; 
+      content: string; 
+      date: Date; 
+      isEdited: boolean; 
+    }[]; 
+  }[] = []; // Initialisation à une liste vide
+
+  constructor(private chatService: ChatService, private authService: AuthService) {
+  } // Correction de "chatServive" en "chatService"
+
+  ngOnInit(): void {
+    // Chargement des données des chats depuis le service
+    this.chats = this.chatService.getAllChats(); // Ajout des parenthèses pour appeler la méthode
   }
 
+  // Inverse la visibilité de app-friends
   toggleFriends(): void {
-    this.showFriends = !this.showFriends;  // Inverse la visibilité de app-friends
+    this.showFriends = !this.showFriends;
+  }
+
+  // Inverse la visibilité de app-messages
+  toggleMessages(): void {
+    this.showMessages = !this.showMessages;
+  }
+
+  isCurrentUser(chat: { id: number; name: string; users: { id: number; username: string }[] }): boolean {
+    const currentUserId = this.authService.getCurrentUserId();
+    return chat.users.some(user => user.id === currentUserId);
   }
 }
